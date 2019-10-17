6df8c92df8fc5caeac6baec0fef2b2adad8f9da8
Adam Carter <mail@adam-carter.com>
2019-10-17 11:08:23 -0700

    no clue.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(evt) {
+    evt.preventDefault();
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Container>
+          <Form>
+            <Form.Group as={Row} controlId="GithubAccount">
+              <Col sm="2">
+                <Form.Label>Github account</Form.Label>
+              </Col>
+              <Col sm="10">
+                <Form.Control type="text" placeholder="" />
+              </Col>
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={this.handleClick}>
+              Get info
+          </Button>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
